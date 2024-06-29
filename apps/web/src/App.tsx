bbd65176e790419b126429fdb0ae6cf75f898b5e
Vaibhav <vs630916@gmail.com>
2024-06-29 08:58:34 +0530

    feat: signup page

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,14 +1,20 @@
 import './App.css'
-import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
-import { lazy, Suspense } from "react"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { lazy, Suspense } from "react";
 
-const Home = lazy(() => import('./pages/Home'))
 
-const Navbar = lazy(() => import('./components/Navbar'))
-const Loader = lazy(() => import('./components/Loader'))
-const Footer = lazy(() => import('./components/Footer'))
+const Home = lazy(() => import('./pages/Home'));
+const Navbar = lazy(() => import('./components/Navbar'));
+const Loader = lazy(() => import('./components/Loader'));
+const Footer = lazy(() => import('./components/Footer'));
+const Signup = lazy(() => import('./components/Signup'));
 
 function App() {
+  // const navigate = useNavigate();
+
+  // const handleClose = () => {
+  //   navigate('/');
+  // };
 
   return (
     <Router>
@@ -16,11 +22,12 @@ function App() {
       <Suspense fallback={<Loader />}>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<Signup />} />
         </Routes>
       </Suspense>
       <Footer />
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
